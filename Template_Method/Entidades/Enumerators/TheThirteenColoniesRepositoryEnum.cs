94090a657a40a5d790c10b7ee13969bcd29d4f99
Daniel <danielsantos_ads@hotmail.com>
2021-04-18 19:18:04 -0300

    Revert "Revert "Adding fixed values ​​on overloads.""
    This reverts commit ae4be906b19afbea7491ab8159b696d1d8d10b93.

---
+++ Template_Method/Entidades/Enumerators/TheThirteenColoniesRepositoryEnum.cs
@@ -0,0 +1,22 @@
+﻿namespace Template_Method.Entidades.Enumerators
+{
+    /// <summary>
+    /// I used this enumerator as a class provider attempt.
+    /// </summary>
+    public enum TheThirteenColoniesRepositoryEnum
+    {
+        ConnecticutRepository,
+        DelawareRepository,
+        GeorgiaRepository,
+        MarylandRepository,
+        MassachusettsRepository,
+        NewHampshireRepository,
+        NewJerseyRepository,
+        NewYorkRepository,
+        NorthCarolinaRepository,
+        PensilvaniaRepository,
+        RhodeIslandRepository,
+        SouthCalorinaRepository,
+        VirginiaRepository
+    }
+}
--- Template_Method/Interfaces/ITheThirteenColonies.cs
+++ Template_Method/Interfaces/ITheThirteenColonies.cs
@@ -1,6 +1,11 @@
 ﻿namespace Template_Method.Interfaces
 {
+    /// <summary>
+    /// This interface is implemented in the parent class <see cref="Template_Method.Services.TheThirteenColonies"/> 
+    /// in order to use method <see cref="GetByState()"/> by calling overloaded methods
+    /// </summary>
     public interface ITheThirteenColonies
     {
+        public (int, string) GetByState();
     }
 }
