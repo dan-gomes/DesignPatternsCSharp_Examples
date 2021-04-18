ae4be906b19afbea7491ab8159b696d1d8d10b93
Daniel <danielsantos_ads@hotmail.com>
2021-04-18 19:08:05 -0300

    Revert "Adding fixed values ​​on overloads."
    This reverts commit 82c091e0fb4c5652c372c06cd675d0436dd1c915.

--- Template_Method/Program.cs
+++ Template_Method/Program.cs
@@ -1,58 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Reflection;
-using Template_Method.Entidades.Enumerators;
-using Template_Method.Interfaces;
 
 namespace Template_Method
 {
     public class Program
     {
-        private static IDictionary<TheThirteenColoniesEnum, Type> _repository = new Dictionary<TheThirteenColoniesEnum, Type>();
-
         static void Main(string[] args)
         {
-
-            var repositorys = Enum.GetNames(typeof(TheThirteenColoniesRepositoryEnum));
-            var colonies = Enum.GetNames(typeof(TheThirteenColoniesEnum));
-
-            for (int i = 0; i < repositorys.Length; i++)
-            {
-                _repository[(TheThirteenColoniesEnum)Enum
-                    .Parse(typeof(TheThirteenColoniesEnum), colonies[i])] = Type.GetType($"Template_Method.Entidades.{repositorys[i]}");
-            }
-
-            Console.WriteLine("Informe o enumerador: ");
-            int input = int.Parse(Console.ReadLine());
-
-            var resultado = Create((TheThirteenColoniesEnum)input)
-                .GetByState();
-
-            Console.WriteLine(
-                "Population: " + resultado.Item1.ToString("F2", CultureInfo.InvariantCulture) 
-                + Environment.NewLine
-                + "Description: " + resultado.Item2);
+            Console.WriteLine("Hello World!");
         }
-
-        /// <summary>
-        /// This method will instantiate through the assembly of a class.
-        /// </summary>
-        /// <param name="theThirteenColoniesEnum"></param>
-        /// <returns>A new instance of a class that has the interface <see cref="ITheThirteenColonies"/> </returns>
-        public static ITheThirteenColonies Create(TheThirteenColoniesEnum theThirteenColoniesEnum)
-        {
-            ITheThirteenColonies result = null;
-
-            if (_repository.TryGetValue(theThirteenColoniesEnum, out Type type))
-            {
-                var assembly = Assembly.Load("Template_Method")
-                    .GetTypes()
-                    .First(t => t.Name == type.Name);
-                result = Activator.CreateInstance(assembly) as ITheThirteenColonies;
-            }
-            return result;
-        }      
     }
 }
--- Template_Method/Services/TheThirteenColonies.cs
+++ Template_Method/Services/TheThirteenColonies.cs
@@ -1,30 +1,6 @@
-﻿using Template_Method.Interfaces;
-
-namespace Template_Method.Services
+﻿namespace Template_Method.Services
 {
-    /// <summary>
-    /// Class <see cref="TheThirteenColonies"/> is responsible for implementing o Template Method
-    /// through polymophism.
-    /// </summary>
-    public abstract class TheThirteenColonies : ITheThirteenColonies
+    public class TheThirteenColonies
     {
-        /// <summary>
-        /// Method that will access the overloaded methods in the child classes.         
-        /// </summary>
-        /// <returns></returns>
-        public (int, string) GetByState()        
-            => (Population(), Description());
-
-        /// <summary>
-        /// Method overloaded by child class. 
-        /// </summary>
-        /// <returns></returns>
-        public abstract string Description();
-
-        /// <summary>
-        /// Method overloaded by child class. 
-        /// </summary>
-        /// <returns></returns>
-        public abstract int Population();       
     }
 }
